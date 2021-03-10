92fdc7d0d8dbb6e8e320b985b6e7dbd53952b538
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-03-10 17:54:50 +0400

    All

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def index():
-	return render_template("index.html")
+	#return render_template("index.html")
+	return "index"
 
 
 if __name__=="__main__":
